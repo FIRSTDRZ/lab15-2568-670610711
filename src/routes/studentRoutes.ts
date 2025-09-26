7cfd941badb06f364c84dd7c43000e7e63c46ee4
first <firstgang17@gmail.com>
2025-09-26 09:12:35 +0700

    edited01

--- src/routes/studentRoutes.ts
+++ src/routes/studentRoutes.ts
@@ -1,7 +1,18 @@
-import { Router } from "express";
+import { Router, type Request, type Response } from "express";
 const router = Router();
 
-router.get("/",() => {
+router.get("/",(req: Request, res: Response) => {
+  return res.json({
+    success: true,
+    message: "Student Information",
+    data: {
+        studentId: "670610711",
+        firstName: "Nasapong",
+        lastName: "Thipwong",
+        program: "CPE",
+        section: "001",
+        }
+    });
 });
 
 export default router;
